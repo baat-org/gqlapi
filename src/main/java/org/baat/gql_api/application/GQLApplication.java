f37109a2d7067940e80708b8bbd852bea9e33647
Sachin Goyal <sachin@fanduel.com>
2019-09-01 22:25:48 +0100

    updated gitignore

--- src/main/java/org/baat/gql_api/application/GQLApplication.java
+++ src/main/java/org/baat/gql_api/application/GQLApplication.java
@@ -2,13 +2,9 @@ package org.baat.gql_api.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("org.baat.gql_api.repository")
-@EntityScan("org.baat.gql_api.repository.entity")
 @ComponentScan("org.baat")
 public class GQLApplication {
     public static void main(String[] args) {
